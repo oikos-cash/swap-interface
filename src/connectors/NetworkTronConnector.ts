ab0dcb82b0a8c64da15557f00fbf5a9bc878dff5
Kevin Holder <kevholderx@gmail.com>
2020-08-30 00:43:04 -0400

    tron: remove deadcode

--- src/connectors/NetworkTronConnector.ts
+++ src/connectors/NetworkTronConnector.ts
@@ -7,4 +7,23 @@ export class NetworkTronConnector extends InjectedTronConnector {
     super(kwargs)
     this.provider = createJavaTronProvider()
   }
+
+  async requestProvider(...args: any[]) {
+    const res = await this.provider.request(...args)
+    console.log(res)
+    // TODO: wrap error with throw new NoEthereumProviderError()?
+    return res
+  }
+
+  public async activate(): Promise<any> {
+    return { provider: this.provider }
+  }
+
+  public async getProvider(): Promise<any> {
+    return this.provider
+  }
+
+  public async getAccount(): Promise<null | string> {
+    return null
+  }
 }
--- src/connectors/injected-tron-connector/index.ts
+++ src/connectors/injected-tron-connector/index.ts
@@ -4,25 +4,6 @@ import { AbstractConnectorArguments, ConnectorUpdate } from '@web3-react/types'
 import { AbstractConnector } from '@web3-react/abstract-connector'
 // import warning from 'tiny-warning'
 
-type SendReturnResult = { result: any }
-type SendReturn = any
-
-type Send = (method: string, params?: any[]) => Promise<SendReturnResult | SendReturn>
-// type SendOld = ({ method }: { method: string }) => Promise<SendReturnResult | SendReturn>
-
-interface Ethereum {
-  send: Send
-  enable: () => Promise<string[]>
-  on?: (method: string, listener: (...args: any[]) => void) => void
-  removeListener?: (method: string, listener: (...args: any[]) => void) => void
-}
-
-declare interface Window {
-  ethereum?: Ethereum
-}
-
-declare const __DEV__: boolean
-
 export class NoEthereumProviderError extends Error {
   public constructor() {
     super()
@@ -54,105 +35,34 @@ export class InjectedTronConnector extends AbstractConnector {
     */
   }
 
-  async sendProvider(...args: any[]) {
-    const res = await this.provider.send(...args)
+  async requestProvider(args: any) {
+    const res = await this.provider.request(args)
+    console.log({ res })
     // TODO: wrap error with throw new NoEthereumProviderError()?
-    return res.result
+    return res
   }
-
-  /*
-  private handleChainChanged(chainId: string | number): void {
-    if (__DEV__) {
-      console.log("Handling 'chainChanged' event with payload", chainId)
-    }
-    this.emitUpdate({ chainId, provider: window.ethereum })
-  }
-
-  private handleAccountsChanged(accounts: string[]): void {
-    if (__DEV__) {
-      console.log("Handling 'accountsChanged' event with payload", accounts)
-    }
-    if (accounts.length === 0) {
-      this.emitDeactivate()
-    } else {
-      this.emitUpdate({ account: accounts[0] })
-    }
-  }
-
-  private handleClose(code: number, reason: string): void {
-    if (__DEV__) {
-      console.log("Handling 'close' event with payload", code, reason)
-    }
-    this.emitDeactivate()
-  }
-
-  private handleNetworkChanged(networkId: string | number): void {
-    if (__DEV__) {
-      console.log("Handling 'networkChanged' event with payload", networkId)
-    }
-    this.emitUpdate({ chainId: networkId, provider: window.ethereum })
-  }
-  */
 
   public async activate(): Promise<ConnectorUpdate> {
-    const accounts = await this.sendProvider('eth_requestAccounts')
+    const accounts = await this.requestProvider({ method: 'eth_accounts' })
+    console.log({ accounts })
     const account = accounts[0]
     return { provider: this.provider, account }
   }
-
-  /*
-  public async activate(): Promise<ConnectorUpdate> {
-    if (!window.ethereum) {
-      throw new NoEthereumProviderError()
-    }
-
-    if (window.ethereum.on) {
-      window.ethereum.on('chainChanged', this.handleChainChanged)
-      window.ethereum.on('accountsChanged', this.handleAccountsChanged)
-      window.ethereum.on('close', this.handleClose)
-      window.ethereum.on('networkChanged', this.handleNetworkChanged)
-    }
-
-    if ((window.ethereum as any).isMetaMask) {
-      ;(window.ethereum as any).autoRefreshOnNetworkChange = false
-    }
-
-    // try to activate + get account via eth_requestAccounts
-    let account
-    try {
-      account = await ((window.ethereum as any).send as Send)('eth_requestAccounts').then(
-        sendReturn => parseSendReturn(sendReturn)[0]
-      )
-    } catch (error) {
-      if ((error as any).code === 4001) {
-        throw new UserRejectedRequestError()
-      }
-      warning(false, 'eth_requestAccounts was unsuccessful, falling back to enable')
-    }
-
-    // if unsuccessful, try enable
-    if (!account) {
-      // if enable is successful but doesn't return accounts, fall back to getAccount (not happy i have to do this...)
-      account = await (window.ethereum as Ethereum)
-        .enable()
-        .then(sendReturn => sendReturn && parseSendReturn(sendReturn)[0])
-    }
-
-    return { provider: window.ethereum, ...(account ? { account } : {}) }
-  }
-  */
 
   public async getProvider(): Promise<any> {
     return this.provider
   }
 
   public async getChainId(): Promise<number | string> {
-    const chainId = await this.sendProvider('eth_chainId')
-    return chainId
+    // const chainId = await this.requestProvider({ method: 'eth_chainId' })
+    // return chainId
+    // TODO: TRON: wait for eth_chainId to be implemented in provider
+    console.log('TODO: TRON: eth_chainId')
+    return '1515'
   }
 
   public async getAccount(): Promise<null | string> {
-    const accounts = await this.sendProvider('eth_requestAccounts')
+    const accounts = await this.requestProvider({ method: 'eth_accounts' })
     const account = accounts[0]
     return account
   }
@@ -161,39 +71,8 @@ export class InjectedTronConnector extends AbstractConnector {
     return true
   }
 
-  /*
-  public deactivate() {
-    if (window.ethereum && window.ethereum.removeListener) {
-      window.ethereum.removeListener('chainChanged', this.handleChainChanged)
-      window.ethereum.removeListener('accountsChanged', this.handleAccountsChanged)
-      window.ethereum.removeListener('close', this.handleClose)
-      window.ethereum.removeListener('networkChanged', this.handleNetworkChanged)
-    }
-  }
-   */
-
   public async isAuthorized(): Promise<boolean> {
     // TODO: check if tronlink unlocked?
     return true
   }
-
-  /*
-  public async isAuthorized(): Promise<boolean> {
-    if (!window.ethereum) {
-      return false
-    }
-
-    try {
-      return await ((window.ethereum as Ethereum).send as Send)('eth_accounts').then(sendReturn => {
-        if (parseSendReturn(sendReturn).length > 0) {
-          return true
-        } else {
-          return false
-        }
-      })
-    } catch {
-      return false
-    }
-  }
- */
 }
