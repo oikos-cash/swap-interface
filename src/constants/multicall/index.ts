c542a8dfd6fb919b17e3f33b3d68e7d7eae24e9a
Kevin Holder <kevholderx@gmail.com>
2020-09-04 00:20:22 -0400

    mainnet: multicall contract address

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -3,7 +3,7 @@ import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
   // TODO: TRON: mainnet multicall contract address
-  [ChainId.MAINNET]: '0xtodomulticallmainnet',
+  [ChainId.MAINNET]: '0xD3573a8728A49512A1485D63180Ed5b095e11D5C',
   [ChainId.NILE]: '0x04A6730FC23a5f2C3d94F7C7aCb4F92Eab8282c2',
   [ChainId.SHASTA]: '0xtodomulticallshasta'
 }
